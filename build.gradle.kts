674adaad26fd7922b969e019db622a0ecafac52f
Daim Sharif <daimsharif@hotmail.com>
2024-04-27 16:02:04 +0400

    changing gradle plugin and java version to 11 for abdu

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+buildscript {
+    val agp_version by extra("7.4.2")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "7.4.2" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
